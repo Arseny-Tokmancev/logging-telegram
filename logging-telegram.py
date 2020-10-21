c4f638f7e1f64ac187021f50e486296376cb0a2d
Arseny-Tokmancev <67480258+Arseny-Tokmancev@users.noreply.github.com>
2020-10-21 22:56:09 +0500

    Update logging-telegram.py

--- logging-telegram.py
+++ logging-telegram.py
@@ -8,13 +8,14 @@ _, api_id, api_hash, bot_token, destination_id, *args = sys.argv
 destination_id = int(destination_id)
 client = TelegramClient(MemorySession(), api_id, api_hash).start(bot_token=bot_token)
 journald = Popen(['journalctl', '-f']+args, stdout=PIPE, stderr=PIPE)
+last_message = None
 while journald.poll() == None:
     message = journald.stdout.readline()
-    errors  = None#journald.stderr.readline()
     if message:
         message = message.decode()
-        print(message)
-        client.send_message(destination_id, message)
-    if errors:
-        print('errors:', errors)
-        client.send_message(destination_id, 'errors:'+errors)
+        # print(message)
+        # print(last_message.message)
+        if last_message == None or (len(last_message.message + '\n' + message) > 4096):
+            last_message = client.send_message(destination_id, message)
+        else:
+            last_message = last_message.edit(last_message.message + '\n' + message)
