66be040378907d83cd76d7782f5bc8147cd148ec
Arseny-Tokmancev <67480258+Arseny-Tokmancev@users.noreply.github.com>
2020-10-21 17:43:08 +0500

    Update logging-telegram.py

--- logging-telegram.py
+++ logging-telegram.py
@@ -1,5 +1,10 @@
-import subprocess, sys
-from telethon import TelegramClient, MemorySession
+from subprocess import Popen, PIPE
+
+import sys
+from telethon.sync import TelegramClient, MemorySession
 
 destination_id, bot_token, api_id, api_hash, *args = sys.argv
-client = await TelegramClient(MemorySession(), api_id, api_hash).start(bot_token=bot_token)
+client = TelegramClient(MemorySession(), api_id, api_hash).start(bot_token=bot_token)
+journald = Popen(['journalctl']+args, stdout=PIPE, stderr=PIPE)
+while journald.poll() == None:
+    client.send_message(destination_id, journald.read())
